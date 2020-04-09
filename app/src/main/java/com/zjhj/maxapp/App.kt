72f1dc084a384b7c46cf71812343933448c39c65
yingtian648@163.com <yingtian648@163.com>
2020-04-09 14:25:06 +0800

    添加配置

---
+++ app/src/main/java/com/zjhj/maxapp/App.kt
@@ -0,0 +1,17 @@
+package com.zjhj.maxapp
+
+import android.app.Application
+
+/**
+ * CreateTime 2020/4/9 09:40
+ * Author LiuShiHua
+ * Description：
+ */
+class App : Application(){
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+
+}
